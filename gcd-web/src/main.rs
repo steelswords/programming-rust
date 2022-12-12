d8c73beeb6bf4e56b42007ffea30dea14a4669e5
Tristan Andrus <tristan@tristanandrus.com>
2022-12-12 11:23:48 -0700

    Add GCD calculation to gcd-web
    Signed-off-by: Tristan Andrus <tristan@tristanandrus.com>

--- gcd-web/src/main.rs
+++ gcd-web/src/main.rs
@@ -1,10 +1,22 @@
+// From "Programming Rust"
+// Modified by Tristan Andrus
+
 use actix_web::{web, App, HttpResponse, HttpServer};
+use serde::Deserialize;
+mod gcd;
+
+#[derive(Deserialize)]
+struct GcdParameters {
+    n: u64,
+    m: u64,
+}
 
 fn main() {
     println!("Hello, world!");
     let server = HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(get_index))
+            .route("/gcd", web::post().to(post_gcd))
     });
 
     println!("Serving on localhost:4000...");
@@ -26,4 +38,20 @@ fn get_index() -> HttpResponse {
                 </form>
             "#,
         )
+}
+
+fn post_gcd(form: web::Form<GcdParameters>) -> HttpResponse {
+    if form.n == 0 || form.m == 0 {
+        return HttpResponse::BadRequest()
+            .content_type("text/html")
+            .body("Computing the GCD with zero is boring.");
+    }
+
+    let response = format!("The greatest common divisor of the numbers {} and {} \
+                            is <b>{}</b>",
+                           form.n, form.m, gcd::gcd(form.n, form.m));
+
+    HttpResponse::Ok()
+        .content_type("text/html")
+        .body(response)
 }
