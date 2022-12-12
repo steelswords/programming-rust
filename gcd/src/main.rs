b17bb0464ced8acfd0d5d64656f8001b003fef43
Tristan Andrus <tristan@tristanandrus.com>
2022-12-11 21:57:10 -0700

    Add gcd program
    Signed-off-by: Tristan Andrus <tristan@tristanandrus.com>

---
+++ gcd/src/main.rs
@@ -0,0 +1,39 @@
+// From "Programming Rust", p. 11
+// Typed and slightly modified by Tristan Andrus
+
+use std::str::FromStr;
+use std::env;
+
+fn main() {
+    let mut numbers = Vec::new();
+
+    for arg in env::args().skip(1) {
+        numbers.push(u64::from_str(&arg)
+                     .expect("Error! Argument cannot parse to u64."));
+    }
+
+    if numbers.len() == 0 {
+        eprintln!("Usage: gcd NUMBER1 NUMBER2 ...");
+        std::process::exit(1);
+    }
+
+    let mut d = numbers[0];
+    for m in &numbers[1..] {
+        d = gcd(d, *m);
+    }
+
+    println!("The greatest common divisor of {:?} is {}", numbers, d);
+}
+
+fn gcd(mut n: u64, mut m: u64) -> u64 {
+    assert!(n != 0 && m != 0);
+    while m != 0 {
+        if m < n {
+            let t = m;
+            m = n;
+            n = t;
+        }
+        m = m % n;
+    }
+    n
+}
